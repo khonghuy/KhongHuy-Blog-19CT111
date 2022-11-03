44a0c8d20cba09c404a3e47bbf5649546fa344cb
khonghuy <75528623+khonghuy@users.noreply.github.com>
2022-11-03 15:37:32 +0700

    Update UserRepository.cs

--- MyBlog/Repository/UserRepository.cs
+++ MyBlog/Repository/UserRepository.cs
@@ -28,7 +28,7 @@ namespace MyBlog.Repository
                 Address = user.Address,
                 DateOfBirth = user.DateOfBirth,
             };
-            return result;hello1
+            return result;
         }
     }
 }
